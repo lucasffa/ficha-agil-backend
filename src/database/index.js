7830a41b8692304561012499f034194ed728f044
lucasffa <113642919+lucasffa@users.noreply.github.com>
2023-11-28 20:32:38 -0300

    Updated index.js
    Now linking to process.env variables.

--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'db_itaka',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
 
 exports.query = async (query, values) => {
